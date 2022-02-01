1ce17ea5fd76503b803b6138f38cfddbc4dc2abd
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-02-01 07:15:52 +0000

    [Finish]課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,18 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    # ログインしているidを投稿するユーザのidに入れてあげないと投稿している人が誰かわからなくなる
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -20,6 +24,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 
   def update
@@ -31,15 +38,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
