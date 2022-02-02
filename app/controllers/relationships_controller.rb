81f55a47eb8403b75c7f650166996b91985690c8
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-02-02 11:37:01 +0000

    [Finish]課題４

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,20 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    following = current_user.relationships.build(follower_id: params[:user_id])
+    # following_idにはcurrent_user.idが入る
+    # followerにはパラメーターから取ってきたuser_idが入る
+    following.save
+    redirect_to request.referrer || root_path
+  end
+
+  def destroy
+    following = current_user.relationships.find_by(follower_id: params[:user_id])
+    # following_idにはcurrent_user.idが入る
+    # followerにはパラメーターから取ってきたuser_idが入る
+    following.destroy
+    redirect_to request.referrer || root_path
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, foreign_key: :following_id
+  has_many :followings, through: :relationships, source: :follower
+  # あるユーザーがフォローしている人をrelationshipsテーブルを介して取ってくる
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :following
+  # あるユーザーをフォローしている人をrelationshipテーブルを介して取ってくる
+  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -16,4 +23,12 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+  def is_followed_by?(user)
+    # 引数に渡されたユーザーにフォローされているかどうかの判定をする
+    reverse_of_relationships.find_by(following_id: user.id).present?
+    # reverse_of_relationshipsはfollowerを指している。
+    # following_id: user.idでuseridをfollowingに代入
+  end
+  
 end
