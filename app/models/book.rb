6d5cace11bd60045d8813a8ceebc3f01a7658f88
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-02-03 01:53:35 +0000

    [Finish]課題5

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,21 +5,21 @@ class Book < ApplicationRecord
 
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
-  
+
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(searches,words)
     if searches == "perfect_match"
-      @book = Book.where("name LIKE ?", "#{words}")
+      @book = Book.where("title LIKE(?) OR body LIKE(?)", "#{words}", "#{words}")
     elsif searches == "forward_match"
-      @book = Book.where("name LIKE ?", "#{words}%")
+      @book = Book.where("title LIKE(?) OR body LIKE(?)", "#{words}%", "#{words}%")
     elsif searches == "backward_match"
-      @book = Book.where("name LIKE ?", "%#{words}")
+      @book = Book.where("title LIKE(?) OR body LIKE(?)", "%#{words}", "%#{words}")
     elsif searches == "partial_match"
-      @book = Book.where("name LIKE ?", "%#{words}%")
+      @book = Book.where("title LIKE(?) OR body LIKE(?)", "%#{words}%", "%#{words}%")
     else
       @book = Book.all
     end
