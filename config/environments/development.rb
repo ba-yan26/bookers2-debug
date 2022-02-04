cb7b64193228a18febb16215e9c2d8a339c9f76b
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-02-04 08:52:06 +0000

    [Finish]課題9c

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,18 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "8f9ccd1733a54b5990c340d7f78ca887.vfs.cloud9.us-east-1.amazonaws.com"
+
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:            ENV['KEY'],
+    password:             ENV['SECRET_KEY'],
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+
 end
