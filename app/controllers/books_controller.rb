d9d732aad45f6df0847f04370e7aa12f8f77687b
EC2 Default User <ec2-user@ip-172-31-30-202.ec2.internal>
2022-02-07 04:20:37 +0000

    [Finish],課題7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,30 @@ class BooksController < ApplicationController
 
   def index
     to = Time.current.at_end_of_day
+    # Time.currentはrailsのapplication.rbのconfig.time_zoneをみている
+    # 要は現在の時刻をとってきている
+    # at_end_of_dayは1日の終わりを23:59に設定している
     from = (to - 6.day).at_beginning_of_day
+    # (to - 6.day)は上記記述のtoに入っている時刻から前６日間
+    # at_beginning_of_dayは時刻を1日の始まりの0:00に設定
+
+    # 上記コードを要約すると
+    # 6日前の0:00から今日の23:59までのデータを取ってくるという意味になる
     @books = Book.includes(:favorited_users).
-      sort {|a,b|
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }
+    # bookモデルからデータを取得するときにfavorited_usersのデータもまとめて取得する
+    # Book.allでも全データ取り出せるが、都度どのユーザーが投稿したのか取りに行かないといけない
+    # includesを書くことでfavorited_usersテーブルのデータを事前に読み込まれているため無駄にSQL文が発行されない
+      # sort {|a,b|
+      # sortメソッドは降順、昇順の並び替えをするメソッド
+      # 普通にsortすると2よりも10,11の方が小さいと判定されてしまうのでブロック|a,b|を渡してあげる
+        # a.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
+        # b.favorited_users.includes(:favorites).where(created_at: from...to).size
+        # a,b両辺(aが左辺,bが右辺)の値を比較していいね数を並び替えている
+      # }.reverse
+
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
     @book = Book.new
   end
 
